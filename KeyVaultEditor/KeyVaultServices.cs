4ac81b0e8be637e71ff995bff5aa6d02f2624619
Roderic Bos <robos@deloitte.nl>
2022-01-15 16:53:44 +0100

    dynamic keyvault

--- KeyVaultEditor/KeyVaultServices.cs
+++ KeyVaultEditor/KeyVaultServices.cs
@@ -3,54 +3,58 @@ using Azure.Security.KeyVault.Secrets;
 
 namespace KeyVaultEditor
 {
-    public class KeyVaultServices
-    {
-        private readonly SecretClient secretClient;
-        private readonly ILogger<KeyVaultServices> logger;
-        public string Url { get; }
+   public class KeyVaultServices
+   {
+      private SecretClient? secretClient;
+      private readonly ILogger<KeyVaultServices> logger;
+      public string? Url { get; set;}
 
-        public KeyVaultServices( ILogger<KeyVaultServices> logger, IConfiguration configuration)
-        {
-            this.logger = logger;
-            Url = configuration["AzureKeyVaultUrl"];
-            logger.LogInformation("Accessing {keyvault}", Url);
-            secretClient = new SecretClient(new Uri(Url), new DefaultAzureCredential());
-        }
+      public KeyVaultServices(ILogger<KeyVaultServices> logger)
+      {
+         this.logger = logger;
+      }
 
-        public async Task<bool> DeleteSecretValue(string name)
-        {
-            var op = await secretClient.StartDeleteSecretAsync(name);
-            var res = await op.WaitForCompletionAsync();
+      public void SetSecretClient(Uri url)
+      {
+         Url = url.AbsoluteUri;
+         logger.LogInformation("Accessing {keyvault}", url);
+         secretClient = new SecretClient(url, new DefaultAzureCredential());
+      }
 
-            return res?.Value?.Name == name;
-        }
+      public async Task<bool> DeleteSecretValue(string name)
+      {
+         var op = await secretClient.StartDeleteSecretAsync(name);
+         var res = await op.WaitForCompletionAsync();
 
-        public async Task<IList<KeyVaultSecret>> GetAllSecretsAsync()
-        {
-            var secrets = new List<KeyVaultSecret>();
-            var secretProperties = secretClient.GetPropertiesOfSecretsAsync();
+         return res?.Value?.Name == name;
+      }
 
-            await foreach (var secretProperty in secretProperties)
-            {
-                var response = await secretClient.GetSecretAsync(secretProperty.Name);
-                secrets.Add(response);
-            }
+      public async Task<IList<KeyVaultSecret>> GetAllSecretsAsync()
+      {
+         var secrets = new List<KeyVaultSecret>();
+         var secretProperties = secretClient.GetPropertiesOfSecretsAsync();
 
-            return secrets;
-        }
+         await foreach (var secretProperty in secretProperties)
+         {
+            var response = await secretClient.GetSecretAsync(secretProperty.Name);
+            secrets.Add(response);
+         }
 
-        public async Task<(bool,string?)> StoreNewKeyVaultSecretValue(string name, string value)
-        {
-            try
-            {
-                var secret = await secretClient.SetSecretAsync(name, value);
-                return (secret != null, null);
-            }
-            catch (Azure.RequestFailedException rfe)
-            {
-                logger.LogError(rfe, "Failed to add {name}", name);
-                return (false, rfe.Message);
-            }
-        }
-    }
+         return secrets;
+      }
+
+      public async Task<(bool, string?)> StoreNewKeyVaultSecretValue(string name, string value)
+      {
+         try
+         {
+            var secret = await secretClient.SetSecretAsync(name, value);
+            return (secret != null, null);
+         }
+         catch (Azure.RequestFailedException rfe)
+         {
+            logger.LogError(rfe, "Failed to add {name}", name);
+            return (false, rfe.Message);
+         }
+      }
+   }
 }
--- KeyVaultEditor/Pages/Index.cshtml.cs
+++ KeyVaultEditor/Pages/Index.cshtml.cs
@@ -3,10 +3,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace KeyVaultEditor.Pages
 {
-    public class IndexModel : PageModel
-    {       
-        public void OnGet()
-        {
-        }
-    }
+   public class IndexModel : PageModel
+   {
+      public KeyVaultServices KeyVaultService { get; }
+
+      public IndexModel(KeyVaultServices keyVaultService)
+      {
+         KeyVaultService = keyVaultService;
+      }
+
+      public IActionResult OnPostGotoEdit()
+      {
+         KeyVaultService.SetSecretClient(new Uri(Request.Form["url"]));
+         return RedirectToPage("KeyVault");
+      }
+   }
 }
