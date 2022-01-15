9bfa71fb79c9e95799e724f0327f7b2bbda23f90
Roderic Bos <robos@deloitte.nl>
2022-01-15 15:25:20 +0100

    cleanup

--- KeyVaultEditor/wwwroot/js/site.js
+++ KeyVaultEditor/wwwroot/js/site.js
@@ -1,39 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
 var allSaved = document.querySelectorAll('tr.saved,tr.failed');
 
 if (allSaved.length > 0) {
-    console.log('saved');
-    for (let i = 0; i < allSaved.length; i++) {
-        const saved = allSaved[i];
-        setTimeout(() => {
-            saved.classList += '-light';
+   console.log('saved');
+   for (let i = 0; i < allSaved.length; i++) {
+      const saved = allSaved[i];
+      setTimeout(() => {
+         saved.classList += '-light';
+         setTimeout(() => {
+            saved.classList += '-er';
             setTimeout(() => {
-                saved.classList += 'er';
-                setTimeout(() => {
-                    saved.classList = null;
-                }, 3000);
+               saved.classList = null;
             }, 3000);
-        }, 2000);
-    }
+         }, 3000);
+      }, 2000);
+   }
 }
 
 var dismisses = document.querySelectorAll('[data-dismiss]');
 for (let i = 0; i < dismisses.length; i++) {
-    var dismiss = dismisses[i];
-    dismiss.addEventListener('click', (e) => {
-        document.querySelector('.modal.show').classList = "modal";
-    });
+   var dismiss = dismisses[i];
+   dismiss.addEventListener('click', (e) => {
+      document.querySelector('.modal.show').classList = "modal";
+   });
 }
 
 var pwInputs = document.querySelectorAll('input[type="password"]');
 for (let i = 0; i < pwInputs.length; i++) {
-    var input = pwInputs[i];
-    input.addEventListener('click', (e) => {
-       if (e.ctrlKey) {
-         e.target.type =  e.target.type == 'text' ? 'password' : 'text';
-       }
-    });
+   var input = pwInputs[i];
+   input.title = 'Ctrl+click to show'
+   input.addEventListener('click', (e) => {
+      if (e.ctrlKey) {
+         e.target.type = e.target.type == 'text' ? 'password' : 'text';
+      }
+   });
 }
