9dab07ea3d4171c0bdeda115f6c33b84de4a04fc
Roderic Bos <robos@deloitte.nl>
2022-01-15 16:36:00 +0100

    visual updates

--- KeyVaultEditor/wwwroot/js/site.js
+++ KeyVaultEditor/wwwroot/js/site.js
@@ -1,31 +1,52 @@
-﻿
-var allSaved = document.querySelectorAll('tr.saved,tr.failed');
-
-if (allSaved.length > 0) {
-   console.log('saved');
-   for (let i = 0; i < allSaved.length; i++) {
-      const saved = allSaved[i];
-      setTimeout(() => {
-         saved.className = '';
-      }, 2000);
+﻿{
+   //remember scroll position on save
+   const allSaveButtons = document.querySelectorAll('button[data-save]');
+   for (let i = 0; i < allSaveButtons.length; i++) {
+      const button = allSaveButtons[i];
+      button.addEventListener('click', (e) => {
+         localStorage.setItem('scrollHeight', window.scrollY);
+      })
+   }
+   const curHeight = localStorage.getItem('scrollHeight');
+   if (curHeight) {
+      window.scrollTo(0, curHeight);
+      localStorage.removeItem('scrollHeight');
    }
 }
 
-var dismisses = document.querySelectorAll('[data-dismiss]');
-for (let i = 0; i < dismisses.length; i++) {
-   var dismiss = dismisses[i];
-   dismiss.addEventListener('click', (e) => {
-      document.querySelector('.modal.show').classList = "modal";
-   });
-}
-
-var pwInputs = document.querySelectorAll('input[type="password"]');
-for (let i = 0; i < pwInputs.length; i++) {
-   var input = pwInputs[i];
-   input.title = 'Ctrl+click to show'
-   input.addEventListener('click', (e) => {
-      if (e.ctrlKey) {
-         e.target.type = e.target.type == 'text' ? 'password' : 'text';
+{
+   //remove saved class
+   const allSaved = document.querySelectorAll('tr.saved');
+   if (allSaved.length > 0) {
+      console.log('saved');
+      for (let i = 0; i < allSaved.length; i++) {
+         const saved = allSaved[i];
+         setTimeout(() => {
+            saved.className = '';
+         }, 2000);
       }
-   });
+   }
+}
+{
+   //dismiss modal error message
+   const dismisses = document.querySelectorAll('[data-dismiss]');
+   for (let i = 0; i < dismisses.length; i++) {
+      const dismiss = dismisses[i];
+      dismiss.addEventListener('click', (e) => {
+         document.querySelector('.modal.show').classList = "modal";
+      });
+   }
+}
+{
+   //add ctrl+click to password fields to show/hide them
+   const pwInputs = document.querySelectorAll('input[type="password"]');
+   for (let i = 0; i < pwInputs.length; i++) {
+      const input = pwInputs[i];
+      input.title = 'Ctrl+click to show'
+      input.addEventListener('click', (e) => {
+         if (e.ctrlKey) {
+            e.target.type = e.target.type == 'text' ? 'password' : 'text';
+         }
+      });
+   }
 }
