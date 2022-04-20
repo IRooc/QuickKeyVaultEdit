7ca7e0e83f63709ae580075f3c5cb70750fa79c9
IRooc <github@rooc.nl>
2022-04-20 09:14:56 +0200

    remember keyvault urls in localstorage

--- KeyVaultEditor/wwwroot/js/site.js
+++ KeyVaultEditor/wwwroot/js/site.js
@@ -38,15 +38,60 @@
    }
 }
 {
-   //add ctrl+click to password fields to show/hide them
-   const pwInputs = document.querySelectorAll('input[type="password"]');
-   for (let i = 0; i < pwInputs.length; i++) {
-      const input = pwInputs[i];
-      input.title = 'Ctrl+click to show'
-      input.addEventListener('click', (e) => {
-         if (e.ctrlKey) {
-            e.target.type = e.target.type == 'text' ? 'password' : 'text';
-         }
-      });
-   }
+    //add ctrl+click to password fields to show/hide them
+    const pwInputs = document.querySelectorAll('input[type="password"]');
+    for (let i = 0; i < pwInputs.length; i++) {
+        const input = pwInputs[i];
+        input.title = 'Ctrl+click to show'
+        input.addEventListener('click', (e) => {
+            if (e.ctrlKey) {
+                e.target.type = e.target.type == 'text' ? 'password' : 'text';
+            }
+        });
+    }
+}
+{
+    //remember previous items
+    const gotoEditButton = document.getElementById('gotoEdit');
+    const storedItemsDiv = document.getElementById('storedItemsDiv');
+    let storedItems = [];
+
+
+    const selectItem = (e) => {
+        const url = e.target.innerText;
+        if (e.ctrlKey) {
+            const ix = storedItems.indexOf(url);
+            if (ix >= 0) {
+                storedItems.splice(ix, 1);
+                console.log('removed',ix, storedItems);
+                localStorage.setItem('storedItems', JSON.stringify(storedItems));
+            }
+            e.target.remove();
+        } else {
+            document.querySelector('input[name="url"]').value = url;
+            gotoEditButton.click();
+        }
+    }
+
+    if (storedItemsDiv) {
+        storedItems = JSON.parse(localStorage.getItem('storedItems') ?? "[]");
+        for (let i = 0; i < storedItems.length; i++) {
+            const el = document.createElement('button');
+            el.type = 'button';
+            el.title = 'Ctrl click to remove';
+            el.onclick = selectItem;
+            el.innerText = storedItems[i];
+            storedItemsDiv.appendChild(el);
+        }
+    }
+    if (gotoEditButton) {
+        gotoEditButton.addEventListener('click', () => {
+            const newUrl = document.querySelector('input[name="url"]').value;
+            if (storedItems.indexOf(newUrl) === -1) {
+                storedItems.push(newUrl);
+                localStorage.setItem('storedItems', JSON.stringify(storedItems));
+            }
+        })
+    }
+
 }
